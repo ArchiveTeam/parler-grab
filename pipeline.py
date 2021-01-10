4d22ff5fa05ee13cb7f1ee4b8828ce5dccea06bf
arkiver <arkiver@protonmail.com>
2021-01-10 14:57:09 +0100

    Version 20210110.01.

--- pipeline.py
+++ pipeline.py
@@ -52,7 +52,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20201225.02'
+VERSION = '20210110.01'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'parler'
 TRACKER_HOST = 'trackerproxy.archiveteam.org'
